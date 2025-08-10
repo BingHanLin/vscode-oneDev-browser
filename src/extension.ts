a4aa5f38214ed625923e91998c9c99142cc1a54c
Binghan Lin <binghan.lin@shonCloud.com>
2025-08-11 00:26:00 +0800

    Add command for React web view and update activation logic

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,9 @@
-import { PRsWebviewViewProvider } from "./prsWebviewViewProvider";
-
 import * as vscode from "vscode";
 
-import { IssuesTreeDataProvider } from "./issuesTreeDataProvider"; // Keep import
-import { BuildsTreeDataProvider } from "./buildsTreeDataProvider"; // Keep import
+
+import { PRsWebviewViewProvider } from "./prsWebviewViewProvider";
+import { IssuesTreeDataProvider } from "./issuesTreeDataProvider";
+import { BuildsTreeDataProvider } from "./buildsTreeDataProvider";
 
 
 
@@ -15,11 +15,41 @@ export function activate(context: vscode.ExtensionContext) {
             new PRsWebviewViewProvider(context)
         )
     );
+
     // Issues/Builds TreeView
     const issuesProvider = new IssuesTreeDataProvider();
     const buildsProvider = new BuildsTreeDataProvider();
     vscode.window.createTreeView("onedevIssuesView", { treeDataProvider: issuesProvider });
     vscode.window.createTreeView("onedevBuildsView", { treeDataProvider: buildsProvider });
+
+
+    {
+
+        let webview = vscode.commands.registerCommand('onedev-browser.testit', () => {
+
+            let panel = vscode.window.createWebviewPanel("webview", "React Test", vscode.ViewColumn.One, {
+                enableScripts: true
+            })
+
+            let scriptSrc = panel.webview.asWebviewUri(vscode.Uri.joinPath(context.extensionUri, "web", "dist", "index.js"))
+            let cssSrc = panel.webview.asWebviewUri(vscode.Uri.joinPath(context.extensionUri, "web", "dist", "index.css"))
+
+            panel.webview.html = `<!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <link rel="stylesheet" href="${cssSrc}" />
+          </head>
+          <body>
+            <noscript>You need to enable JavaScript to run this app.</noscript>
+            <div id="root"></div>
+            <script src="${scriptSrc}"></script>
+          </body>
+        </html>
+        `
+        });
+
+        context.subscriptions.push(webview);
+    }
 }
 
 export function deactivate() { }
